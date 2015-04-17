cc2b06aade1a0f76882a1660832b2abbd0512210
Yuriy Pomazansky <pomazansky@gmail.com>
2015-04-17 14:32:14 +0300

    Add propel models

---
+++ src/UACatalog/Models/Base/BlogQuery.php
@@ -0,0 +1,410 @@
+<?php
+
+namespace UACatalog\Models\Base;
+
+use \Exception;
+use \PDO;
+use Propel\Runtime\Propel;
+use Propel\Runtime\ActiveQuery\Criteria;
+use Propel\Runtime\ActiveQuery\ModelCriteria;
+use Propel\Runtime\Collection\ObjectCollection;
+use Propel\Runtime\Connection\ConnectionInterface;
+use Propel\Runtime\Exception\PropelException;
+use UACatalog\Models\Blog as ChildBlog;
+use UACatalog\Models\BlogQuery as ChildBlogQuery;
+use UACatalog\Models\Map\BlogTableMap;
+
+/**
+ * Base class that represents a query for the 'blog' table.
+ *
+ * 
+ *
+ * @method     ChildBlogQuery orderById($order = Criteria::ASC) Order by the id column
+ * @method     ChildBlogQuery orderByTitle($order = Criteria::ASC) Order by the title column
+ * @method     ChildBlogQuery orderByText($order = Criteria::ASC) Order by the text column
+ *
+ * @method     ChildBlogQuery groupById() Group by the id column
+ * @method     ChildBlogQuery groupByTitle() Group by the title column
+ * @method     ChildBlogQuery groupByText() Group by the text column
+ *
+ * @method     ChildBlogQuery leftJoin($relation) Adds a LEFT JOIN clause to the query
+ * @method     ChildBlogQuery rightJoin($relation) Adds a RIGHT JOIN clause to the query
+ * @method     ChildBlogQuery innerJoin($relation) Adds a INNER JOIN clause to the query
+ *
+ * @method     ChildBlog findOne(ConnectionInterface $con = null) Return the first ChildBlog matching the query
+ * @method     ChildBlog findOneOrCreate(ConnectionInterface $con = null) Return the first ChildBlog matching the query, or a new ChildBlog object populated from the query conditions when no match is found
+ *
+ * @method     ChildBlog findOneById(int $id) Return the first ChildBlog filtered by the id column
+ * @method     ChildBlog findOneByTitle(string $title) Return the first ChildBlog filtered by the title column
+ * @method     ChildBlog findOneByText(string $text) Return the first ChildBlog filtered by the text column *
+
+ * @method     ChildBlog requirePk($key, ConnectionInterface $con = null) Return the ChildBlog by primary key and throws \Propel\Runtime\Exception\EntityNotFoundException when not found
+ * @method     ChildBlog requireOne(ConnectionInterface $con = null) Return the first ChildBlog matching the query and throws \Propel\Runtime\Exception\EntityNotFoundException when not found
+ *
+ * @method     ChildBlog requireOneById(int $id) Return the first ChildBlog filtered by the id column and throws \Propel\Runtime\Exception\EntityNotFoundException when not found
+ * @method     ChildBlog requireOneByTitle(string $title) Return the first ChildBlog filtered by the title column and throws \Propel\Runtime\Exception\EntityNotFoundException when not found
+ * @method     ChildBlog requireOneByText(string $text) Return the first ChildBlog filtered by the text column and throws \Propel\Runtime\Exception\EntityNotFoundException when not found
+ *
+ * @method     ChildBlog[]|ObjectCollection find(ConnectionInterface $con = null) Return ChildBlog objects based on current ModelCriteria
+ * @method     ChildBlog[]|ObjectCollection findById(int $id) Return ChildBlog objects filtered by the id column
+ * @method     ChildBlog[]|ObjectCollection findByTitle(string $title) Return ChildBlog objects filtered by the title column
+ * @method     ChildBlog[]|ObjectCollection findByText(string $text) Return ChildBlog objects filtered by the text column
+ * @method     ChildBlog[]|\Propel\Runtime\Util\PropelModelPager paginate($page = 1, $maxPerPage = 10, ConnectionInterface $con = null) Issue a SELECT query based on the current ModelCriteria and uses a page and a maximum number of results per page to compute an offset and a limit
+ *
+ */
+abstract class BlogQuery extends ModelCriteria
+{
+    protected $entityNotFoundExceptionClass = '\\Propel\\Runtime\\Exception\\EntityNotFoundException';
+
+    /**
+     * Initializes internal state of \UACatalog\Models\Base\BlogQuery object.
+     *
+     * @param     string $dbName The database name
+     * @param     string $modelName The phpName of a model, e.g. 'Book'
+     * @param     string $modelAlias The alias for the model in this query, e.g. 'b'
+     */
+    public function __construct($dbName = 'default', $modelName = '\\UACatalog\\Models\\Blog', $modelAlias = null)
+    {
+        parent::__construct($dbName, $modelName, $modelAlias);
+    }
+
+    /**
+     * Returns a new ChildBlogQuery object.
+     *
+     * @param     string $modelAlias The alias of a model in the query
+     * @param     Criteria $criteria Optional Criteria to build the query from
+     *
+     * @return ChildBlogQuery
+     */
+    public static function create($modelAlias = null, Criteria $criteria = null)
+    {
+        if ($criteria instanceof ChildBlogQuery) {
+            return $criteria;
+        }
+        $query = new ChildBlogQuery();
+        if (null !== $modelAlias) {
+            $query->setModelAlias($modelAlias);
+        }
+        if ($criteria instanceof Criteria) {
+            $query->mergeWith($criteria);
+        }
+
+        return $query;
+    }
+
+    /**
+     * Find object by primary key.
+     * Propel uses the instance pool to skip the database if the object exists.
+     * Go fast if the query is untouched.
+     *
+     * <code>
+     * $obj  = $c->findPk(12, $con);
+     * </code>
+     *
+     * @param mixed $key Primary key to use for the query
+     * @param ConnectionInterface $con an optional connection object
+     *
+     * @return ChildBlog|array|mixed the result, formatted by the current formatter
+     */
+    public function findPk($key, ConnectionInterface $con = null)
+    {
+        if ($key === null) {
+            return null;
+        }
+        if ((null !== ($obj = BlogTableMap::getInstanceFromPool((string) $key))) && !$this->formatter) {
+            // the object is already in the instance pool
+            return $obj;
+        }
+        if ($con === null) {
+            $con = Propel::getServiceContainer()->getReadConnection(BlogTableMap::DATABASE_NAME);
+        }
+        $this->basePreSelect($con);
+        if ($this->formatter || $this->modelAlias || $this->with || $this->select
+         || $this->selectColumns || $this->asColumns || $this->selectModifiers
+         || $this->map || $this->having || $this->joins) {
+            return $this->findPkComplex($key, $con);
+        } else {
+            return $this->findPkSimple($key, $con);
+        }
+    }
+
+    /**
+     * Find object by primary key using raw SQL to go fast.
+     * Bypass doSelect() and the object formatter by using generated code.
+     *
+     * @param     mixed $key Primary key to use for the query
+     * @param     ConnectionInterface $con A connection object
+     *
+     * @throws \Propel\Runtime\Exception\PropelException
+     *
+     * @return ChildBlog A model object, or null if the key is not found
+     */
+    protected function findPkSimple($key, ConnectionInterface $con)
+    {
+        $sql = 'SELECT id, title, text FROM blog WHERE id = :p0';
+        try {
+            $stmt = $con->prepare($sql);            
+            $stmt->bindValue(':p0', $key, PDO::PARAM_INT);
+            $stmt->execute();
+        } catch (Exception $e) {
+            Propel::log($e->getMessage(), Propel::LOG_ERR);
+            throw new PropelException(sprintf('Unable to execute SELECT statement [%s]', $sql), 0, $e);
+        }
+        $obj = null;
+        if ($row = $stmt->fetch(\PDO::FETCH_NUM)) {
+            /** @var ChildBlog $obj */
+            $obj = new ChildBlog();
+            $obj->hydrate($row);
+            BlogTableMap::addInstanceToPool($obj, (string) $key);
+        }
+        $stmt->closeCursor();
+
+        return $obj;
+    }
+
+    /**
+     * Find object by primary key.
+     *
+     * @param     mixed $key Primary key to use for the query
+     * @param     ConnectionInterface $con A connection object
+     *
+     * @return ChildBlog|array|mixed the result, formatted by the current formatter
+     */
+    protected function findPkComplex($key, ConnectionInterface $con)
+    {
+        // As the query uses a PK condition, no limit(1) is necessary.
+        $criteria = $this->isKeepQuery() ? clone $this : $this;
+        $dataFetcher = $criteria
+            ->filterByPrimaryKey($key)
+            ->doSelect($con);
+
+        return $criteria->getFormatter()->init($criteria)->formatOne($dataFetcher);
+    }
+
+    /**
+     * Find objects by primary key
+     * <code>
+     * $objs = $c->findPks(array(12, 56, 832), $con);
+     * </code>
+     * @param     array $keys Primary keys to use for the query
+     * @param     ConnectionInterface $con an optional connection object
+     *
+     * @return ObjectCollection|array|mixed the list of results, formatted by the current formatter
+     */
+    public function findPks($keys, ConnectionInterface $con = null)
+    {
+        if (null === $con) {
+            $con = Propel::getServiceContainer()->getReadConnection($this->getDbName());
+        }
+        $this->basePreSelect($con);
+        $criteria = $this->isKeepQuery() ? clone $this : $this;
+        $dataFetcher = $criteria
+            ->filterByPrimaryKeys($keys)
+            ->doSelect($con);
+
+        return $criteria->getFormatter()->init($criteria)->format($dataFetcher);
+    }
+
+    /**
+     * Filter the query by primary key
+     *
+     * @param     mixed $key Primary key to use for the query
+     *
+     * @return $this|ChildBlogQuery The current query, for fluid interface
+     */
+    public function filterByPrimaryKey($key)
+    {
+
+        return $this->addUsingAlias(BlogTableMap::COL_ID, $key, Criteria::EQUAL);
+    }
+
+    /**
+     * Filter the query by a list of primary keys
+     *
+     * @param     array $keys The list of primary key to use for the query
+     *
+     * @return $this|ChildBlogQuery The current query, for fluid interface
+     */
+    public function filterByPrimaryKeys($keys)
+    {
+
+        return $this->addUsingAlias(BlogTableMap::COL_ID, $keys, Criteria::IN);
+    }
+
+    /**
+     * Filter the query on the id column
+     *
+     * Example usage:
+     * <code>
+     * $query->filterById(1234); // WHERE id = 1234
+     * $query->filterById(array(12, 34)); // WHERE id IN (12, 34)
+     * $query->filterById(array('min' => 12)); // WHERE id > 12
+     * </code>
+     *
+     * @param     mixed $id The value to use as filter.
+     *              Use scalar values for equality.
+     *              Use array values for in_array() equivalent.
+     *              Use associative array('min' => $minValue, 'max' => $maxValue) for intervals.
+     * @param     string $comparison Operator to use for the column comparison, defaults to Criteria::EQUAL
+     *
+     * @return $this|ChildBlogQuery The current query, for fluid interface
+     */
+    public function filterById($id = null, $comparison = null)
+    {
+        if (is_array($id)) {
+            $useMinMax = false;
+            if (isset($id['min'])) {
+                $this->addUsingAlias(BlogTableMap::COL_ID, $id['min'], Criteria::GREATER_EQUAL);
+                $useMinMax = true;
+            }
+            if (isset($id['max'])) {
+                $this->addUsingAlias(BlogTableMap::COL_ID, $id['max'], Criteria::LESS_EQUAL);
+                $useMinMax = true;
+            }
+            if ($useMinMax) {
+                return $this;
+            }
+            if (null === $comparison) {
+                $comparison = Criteria::IN;
+            }
+        }
+
+        return $this->addUsingAlias(BlogTableMap::COL_ID, $id, $comparison);
+    }
+
+    /**
+     * Filter the query on the title column
+     *
+     * Example usage:
+     * <code>
+     * $query->filterByTitle('fooValue');   // WHERE title = 'fooValue'
+     * $query->filterByTitle('%fooValue%'); // WHERE title LIKE '%fooValue%'
+     * </code>
+     *
+     * @param     string $title The value to use as filter.
+     *              Accepts wildcards (* and % trigger a LIKE)
+     * @param     string $comparison Operator to use for the column comparison, defaults to Criteria::EQUAL
+     *
+     * @return $this|ChildBlogQuery The current query, for fluid interface
+     */
+    public function filterByTitle($title = null, $comparison = null)
+    {
+        if (null === $comparison) {
+            if (is_array($title)) {
+                $comparison = Criteria::IN;
+            } elseif (preg_match('/[\%\*]/', $title)) {
+                $title = str_replace('*', '%', $title);
+                $comparison = Criteria::LIKE;
+            }
+        }
+
+        return $this->addUsingAlias(BlogTableMap::COL_TITLE, $title, $comparison);
+    }
+
+    /**
+     * Filter the query on the text column
+     *
+     * Example usage:
+     * <code>
+     * $query->filterByText('fooValue');   // WHERE text = 'fooValue'
+     * $query->filterByText('%fooValue%'); // WHERE text LIKE '%fooValue%'
+     * </code>
+     *
+     * @param     string $text The value to use as filter.
+     *              Accepts wildcards (* and % trigger a LIKE)
+     * @param     string $comparison Operator to use for the column comparison, defaults to Criteria::EQUAL
+     *
+     * @return $this|ChildBlogQuery The current query, for fluid interface
+     */
+    public function filterByText($text = null, $comparison = null)
+    {
+        if (null === $comparison) {
+            if (is_array($text)) {
+                $comparison = Criteria::IN;
+            } elseif (preg_match('/[\%\*]/', $text)) {
+                $text = str_replace('*', '%', $text);
+                $comparison = Criteria::LIKE;
+            }
+        }
+
+        return $this->addUsingAlias(BlogTableMap::COL_TEXT, $text, $comparison);
+    }
+
+    /**
+     * Exclude object from result
+     *
+     * @param   ChildBlog $blog Object to remove from the list of results
+     *
+     * @return $this|ChildBlogQuery The current query, for fluid interface
+     */
+    public function prune($blog = null)
+    {
+        if ($blog) {
+            $this->addUsingAlias(BlogTableMap::COL_ID, $blog->getId(), Criteria::NOT_EQUAL);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Deletes all rows from the blog table.
+     *
+     * @param ConnectionInterface $con the connection to use
+     * @return int The number of affected rows (if supported by underlying database driver).
+     */
+    public function doDeleteAll(ConnectionInterface $con = null)
+    {
+        if (null === $con) {
+            $con = Propel::getServiceContainer()->getWriteConnection(BlogTableMap::DATABASE_NAME);
+        }
+
+        // use transaction because $criteria could contain info
+        // for more than one table or we could emulating ON DELETE CASCADE, etc.
+        return $con->transaction(function () use ($con) {
+            $affectedRows = 0; // initialize var to track total num of affected rows
+            $affectedRows += parent::doDeleteAll($con);
+            // Because this db requires some delete cascade/set null emulation, we have to
+            // clear the cached instance *after* the emulation has happened (since
+            // instances get re-added by the select statement contained therein).
+            BlogTableMap::clearInstancePool();
+            BlogTableMap::clearRelatedInstancePool();
+
+            return $affectedRows;
+        });
+    }
+
+    /**
+     * Performs a DELETE on the database based on the current ModelCriteria
+     *
+     * @param ConnectionInterface $con the connection to use
+     * @return int             The number of affected rows (if supported by underlying database driver).  This includes CASCADE-related rows
+     *                         if supported by native driver or if emulated using Propel.
+     * @throws PropelException Any exceptions caught during processing will be
+     *                         rethrown wrapped into a PropelException.
+     */
+    public function delete(ConnectionInterface $con = null)
+    {
+        if (null === $con) {
+            $con = Propel::getServiceContainer()->getWriteConnection(BlogTableMap::DATABASE_NAME);
+        }
+
+        $criteria = $this;
+
+        // Set the correct dbName
+        $criteria->setDbName(BlogTableMap::DATABASE_NAME);
+
+        // use transaction because $criteria could contain info
+        // for more than one table or we could emulating ON DELETE CASCADE, etc.
+        return $con->transaction(function () use ($con, $criteria) {
+            $affectedRows = 0; // initialize var to track total num of affected rows
+            
+            BlogTableMap::removeInstanceFromPool($criteria);
+        
+            $affectedRows += ModelCriteria::delete($con);
+            BlogTableMap::clearRelatedInstancePool();
+
+            return $affectedRows;
+        });
+    }
+
+} // BlogQuery

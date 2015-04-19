5886dfed991b76955b396dda44c27b925fc5b99f
Yuriy Pomazansky <pomazansky@gmail.com>
2015-04-19 13:01:00 +0300

    Update scheme

--- resources/db/generated-sql/default.sql
+++ resources/db/generated-sql/default.sql
@@ -14,7 +14,62 @@ CREATE TABLE `blog`
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(255) NOT NULL,
     `text` TEXT NOT NULL,
-    `img_file` VARCHAR(255) NOT NULL,
+    `image` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+-- ---------------------------------------------------------------------
+-- product
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `product`;
+
+CREATE TABLE `product`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `price` DOUBLE,
+    `description` TEXT NOT NULL,
+    `images` TEXT NOT NULL,
+    `category_id` INTEGER NOT NULL,
+    `manufacturer_id` INTEGER NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `product_fi_904832` (`category_id`),
+    INDEX `product_fi_26eba6` (`manufacturer_id`),
+    CONSTRAINT `product_fk_904832`
+        FOREIGN KEY (`category_id`)
+        REFERENCES `category` (`id`),
+    CONSTRAINT `product_fk_26eba6`
+        FOREIGN KEY (`manufacturer_id`)
+        REFERENCES `manufacturer` (`id`)
+) ENGINE=InnoDB;
+
+-- ---------------------------------------------------------------------
+-- category
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `category`;
+
+CREATE TABLE `category`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `parent_id` INTEGER,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+-- ---------------------------------------------------------------------
+-- manufacturer
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `manufacturer`;
+
+CREATE TABLE `manufacturer`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `url` VARCHAR(255),
+    `shops` TEXT,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 

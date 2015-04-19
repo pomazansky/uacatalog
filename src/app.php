363f74d9b4e9262cd4c6efb7807bae2190bd8101
Yuriy Pomazansky <pomazansky@gmail.com>
2015-04-19 13:22:12 +0300

    Remove WebProfiler and small fixes

--- src/app.php
+++ src/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Silex\Application;
 use Silex\Provider\FormServiceProvider;
 use Silex\Provider\HttpCacheServiceProvider;
 use Silex\Provider\MonologServiceProvider;
@@ -10,11 +11,13 @@ use Silex\Provider\TranslationServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
-use Silex\Provider\WebProfilerServiceProvider;
-use SilexAssetic\AsseticServiceProvider;
+use Silex\Translator;
 use Symfony\Component\Security\Core\Encoder\PlaintextPasswordEncoder;
 use Symfony\Component\Translation\Loader\YamlFileLoader;
 
+/**
+ * @var Application $app
+ */
 $app->register(new HttpCacheServiceProvider());
 
 $app->register(new SessionServiceProvider());
@@ -22,28 +25,32 @@ $app->register(new ValidatorServiceProvider());
 $app->register(new FormServiceProvider());
 $app->register(new UrlGeneratorServiceProvider());
 
-$app->register(new SecurityServiceProvider(), array(
-    'security.firewalls' => array(
-        'admin' => array(
+$app->register(new SecurityServiceProvider(), [
+    'security.firewalls' => [
+        'admin' => [
             'pattern' => '^/',
-            'form'    => array(
+            'form'    => [
                 'login_path'         => '/login',
                 'username_parameter' => 'form[username]',
-                'password_parameter' => 'form[password]',
-            ),
+                'password_parameter' => 'form[password]'
+            ],
             'logout'    => true,
             'anonymous' => true,
-            'users'     => $app['security.users'],
-        ),
-    ),
-));
+            'users'     => $app['security.users']
+        ]
+    ]
+]);
 
-$app['security.encoder.digest'] = $app->share(function ($app) {
+$app['security.encoder.digest'] = $app->share(function () {
     return new PlaintextPasswordEncoder();
 });
 
 $app->register(new TranslationServiceProvider());
-$app['translator'] = $app->share($app->extend('translator', function ($translator, $app) {
+
+$app['translator'] = $app->share($app->extend('translator', function ($translator) {
+    /**
+     * @var Translator $translator
+     */
     $translator->addLoader('yaml', new YamlFileLoader());
 
     $translator->addResource('yaml', __DIR__.'/../resources/locales/fr.yml', 'fr');
@@ -51,30 +58,27 @@ $app['translator'] = $app->share($app->extend('translator', function ($translato
     return $translator;
 }));
 
-$app->register(new MonologServiceProvider(), array(
+$app->register(new MonologServiceProvider(), [
     'monolog.logfile' => __DIR__.'/../resources/log/app.log',
     'monolog.name'    => 'app',
     'monolog.level'   => 300 // = Logger::WARNING
-));
+]);
 
-$app->register(new TwigServiceProvider(), array(
-    'twig.options'        => array(
+$app->register(new TwigServiceProvider(), [
+    'twig.options'        => [
         'cache'            => isset($app['twig.options.cache']) ? $app['twig.options.cache'] : false,
         'strict_variables' => true
-    ),
-    'twig.form.templates' => array('form_div_layout.html.twig', 'common/form_div_layout.html.twig'),
-    'twig.path'           => array(__DIR__ . '/../resources/views')
-));
+    ],
+    'twig.form.templates' => ['form_div_layout.html.twig', 'common/form_div_layout.html.twig'],
+    'twig.path'           => [__DIR__ . '/../resources/views']
+]);
 
 if ($app['debug'] && isset($app['cache.path'])) {
     $app->register(new ServiceControllerServiceProvider());
-//    $app->register(new WebProfilerServiceProvider(), array(
-//        'profiler.cache_dir' => $app['cache.path'].'/profiler',
-//    ));
 }
 
-$app->register(new Propel\Silex\PropelServiceProvider(), array(
+$app->register(new Propel\Silex\PropelServiceProvider(), [
     'propel.config_file' => __DIR__.'/../resources/db/generated-conf/config.php'
-));
+]);
 
 return $app;
